e10f095356a415c25d240c4fd07bfae8ccba19d3
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2024-08-08 23:49:53 +0530

    Create Day-24/Evaluation_of_Postfix_Expression.cpp

---
+++ Day-24/Evaluation_of_Postfix_Expression.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+    //Function to evaluate a postfix expression.
+
+        int evaluatePostfix(string S)
+ {
+        stack<int>st;
+       for(int i=0; i<S.length(); i++){
+           if(S[i]>='0' and S[i]<='9'){
+               st.push(S[i]-'0');
+               
+           }
+           else{
+               int a= st.top();
+               st.pop();
+               int b=st.top();
+               st.pop();
+               
+               switch(S[i]){
+                   case '+':
+                   st.push(b+a);
+                    break;
+                    
+                    case '-':
+                   st.push(b-a);
+                    break;
+                    
+                    case '*':
+                   st.push(b*a);
+                    break;
+                    
+                    case '/':
+                   st.push(b/a);
+                    break;
+               }
+               
+           }
+       }
+       return st.top();
+    }
+};
