34113fa8d8b832de9fc4b340de86e54986171d74
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2024-08-17 20:48:56 +0530

    Create Day-33/Flattening_a_Linked_List.cpp

---
+++ Day-33/Flattening_a_Linked_List.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    // Function which returns the  root of the flattened linked list.
+     Node* merge2Lists(Node* l1, Node* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        Node* head = l1->data <= l2->data? l1 : l2;
+        head->bottom = l1->data <= l2->data ? 
+                 merge2Lists(l1->bottom, l2) : merge2Lists(l1, l2->bottom);
+        return head;
+    }
+    Node *flatten(Node *root) {
+       if(!root or !root->next)return root;
+       return merge2Lists(root, flatten(root->next));
+    }
+};
