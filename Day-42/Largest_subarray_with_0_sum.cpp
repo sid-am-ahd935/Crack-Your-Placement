182d6b593a41be4f71139551ad7a2eefec050f02
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2024-08-26 21:48:03 +0530

    Create Day-42/Largest_subarray_with_0_sum.cpp

---
+++ Day-42/Largest_subarray_with_0_sum.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+    int maxLen(vector<int>&A, int n) {   
+        int sum=0;
+        int maxi=0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            sum+=A[i];
+            if(sum==0){
+                maxi=i+1;
+            }
+            else{
+                if(mp.find(sum)!=mp.end()){
+                    maxi=max(maxi,i-mp[sum]);
+                }
+                else{
+                    mp[sum]=i;
+                }
+            }
+        }
+        return maxi;
+    }
+};
