4ce017416ec7a98b5a34f089429af1eed86f1d95
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2024-08-27 23:24:51 +0530

    Create Day-43/530_Minimum_Absolute_Difference_in_BST.cpp

---
+++ Day-43/530_Minimum_Absolute_Difference_in_BST.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = INT_MAX; 
+
+    void helper(TreeNode* root, int &prev) {
+        if(not root) return; 
+
+        helper(root->left, prev);
+
+        if(prev != -1) {
+            ans = min(ans, abs(root->val - prev));
+        } 
+        prev = root->val;
+
+        helper(root->right, prev);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        int prev = -1;
+        helper(root, prev);
+        return ans;
+    }
+};
