438ad889a81d811b87dbe8a7d27dbcd897c9b3b0
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2024-08-29 21:53:06 +0530

    Create Day-45/70_Climbing_Stairs.cpp

---
+++ Day-45/70_Climbing_Stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    private:
+    int ncr(int n,int r){
+        double sum=1;
+        for(int i = 1; i <= r; i++){
+            sum = sum * (n - r + i) / i;
+        }
+        return (int)sum;
+    }
+public:
+    int climbStairs(int n) {
+       int ans=1;
+      for(int i=1;i<=n;i++){
+          int x=ncr(n-i,i);
+          //cout<<x<<endl;
+          ans=ans+x;
+      }
+      return ans;
+
+    }
+};
